f2e6bd5cd5652cd203fdde80e3bf81751c1ec817
Causality <m.uddner@gmail.com>
2022-01-15 20:04:13 +0100

    live neighbors tests and logic

--- src/main/java/gameOfLife/Board.java
+++ src/main/java/gameOfLife/Board.java
@@ -18,4 +18,31 @@ public class Board {
     public void kill(int x, int y) {
         board[x][y] = 0;
     }
+
+    public String printable() {
+
+        String printableBoard = "";
+        for (int height = 0; height < yAxis; height++) {
+            printableBoard = printableBoard.concat("|");
+            for (int width = 0; width < xAxis; width++) {
+                printableBoard = printableBoard.concat(board[height][width] + " ");
+            }
+            printableBoard = printableBoard.substring(0, printableBoard.length() - 1);
+            printableBoard = printableBoard.concat("|\n");
+        }
+        return printableBoard;
+    }
+
+    public int countLiveNeighbors(int x, int y) {
+        int neighbors = 0;
+        for (int i = 0; i <= xAxis-1; i++) {
+            for (int j = 0; j <= yAxis-1; j++) {
+                if (i>x-2 && i<x+2 && j>y-2 && j<y+2){
+                    neighbors+=board[i][j];
+                }
+            }
+        }
+        neighbors -= board[x][y];
+        return neighbors;
+    }
 }
