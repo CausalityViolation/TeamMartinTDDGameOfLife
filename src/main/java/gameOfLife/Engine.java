45077bb84255b7fb20496ef9c3c299afce7b0a8e
Causality <m.uddner@gmail.com>
2022-01-16 12:41:32 +0100

    replaced "magic numbers" with constant variables for readability

--- src/main/java/gameOfLife/Engine.java
+++ src/main/java/gameOfLife/Engine.java
@@ -1,6 +1,10 @@
 package gameOfLife;
 
 public class Engine {
+
+    static final int TWO_LIVE_NEIGHBORS = 2;
+    static final int THREE_LIVE_NEIGHBORS = 3;
+
     public void evolve(Board board) {
 
         int[][] nextGeneration = new int[board.getX()][board.getY()];
@@ -12,10 +16,10 @@ public class Engine {
 
         for (int width = 0; width < board.getX(); width++) {
             for (int height = 0; height < board.getY(); height++) {
-                if (nextGeneration[width][height]<2 || nextGeneration[width][height]>3) {
+                if (nextGeneration[width][height]<TWO_LIVE_NEIGHBORS || nextGeneration[width][height]>THREE_LIVE_NEIGHBORS) {
                     board.kill(width,height);
                 }
-                if (nextGeneration[width][height]==3) {
+                if (nextGeneration[width][height]==THREE_LIVE_NEIGHBORS) {
                     board.birth(width, height);
                 }
             }
